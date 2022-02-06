2a49a2e856f09ab9d26f26c8d0fad524a3f3d72a
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-02-06 20:21:05 +0100

    Updates day's work

--- get-online-data.R
+++ get-online-data.R
@@ -3,3 +3,5 @@ source("const.R")
 
 df_paper <- read_sheet(ss = GS_SHEET, sheet = "Original-paper",
                       na=c("na", "nr"))
+
+df_online <- read_sheet(ss = GS_SHEET, sheet = "Original-online")
