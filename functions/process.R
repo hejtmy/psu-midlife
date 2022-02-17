3ecc87e4062e8a06d6444d70dc83120fea64b66f
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-02-17 12:49:50 +0100

    Adds panas processing

--- functions/process.R
+++ functions/process.R
@@ -1,7 +1,8 @@
 process_data <- function(df_all){
   df_all <- df_all %>%
     process_demographics() %>%
-    process_swls()
+    process_swls() %>%
+    process_panas()
   return(df_all)
 }
 
@@ -22,11 +23,23 @@ process_demographics <- function(df_all){
   return(df_all)
 }
 
+process_panas <- function(df_all){
+  # The PANAS online has a bug with quesiton 13 missing and going to 21. So all 
+  # questions from 13 and up need to be one higher
+  df_all <- df_all %>%
+    mutate(panas_s_positive = (panas_01 + panas_03 + panas_05 + panas_09 + panas_10 + 
+                                 panas_12 + panas_15 + panas_17 + panas_18 + panas_20) / 10,
+           panas_s_negative = (panas_02 + panas_04 + panas_06 + panas_07 + panas_08 + 
+                                 panas_11 + panas_14 + panas_16 + panas_19 + panas_21) / 10,
+           panas_s_hedonic = panas_s_positive - panas_s_negative)
+  return(df_all)
+}
+
 ## SWLS ------
 process_swls <- function(df_all){
   #SATISFACTION (swls_01 + swls_02 +…+ swls_05)/ 5
   df_all <- df_all %>% 
-    mutate(swls_satifaction = swls_01 + swls_02 + swls_03 + swls_04 + swls_05)
+    mutate(swls_s_satifaction = (swls_01 + swls_02 + swls_03 + swls_04 + swls_05) / 5)
   return(df_all)
 }
 
