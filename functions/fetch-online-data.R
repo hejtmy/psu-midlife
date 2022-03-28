bffe3425475071601624339003b2a2829278d28e
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-03-28 16:40:46 +0200

    Adds fetching of edited files

--- functions/fetch-online-data.R
+++ functions/fetch-online-data.R
@@ -7,24 +7,32 @@ fetch_data_paper <- function(){
   return(df_paper)
 }
 
+fetch_data_paper_edited <- function(){
+  return(fetch_sheet("Edited-paper"))
+}
+
 fetch_data_online <- function(){
-  df_online <- read_sheet(ss = GS_SHEET, sheet = "Original-online")
-  return(df_online)
+  return(fetch_sheet("Original-online"))
+}
+fetch_data_online_edited <- function(){
+  return(fetch_sheet("Edited-online"))
 }
 
 fetch_question_categories <- function(){
-  df <- read_sheet(GS_SHEET, sheet = "Question-categories")
-  return(df)
+  return(fetch_sheet("Question-categories"))
 }
 
 fetch_error_data <- function(){
-  df <- read_sheet(GS_SHEET, sheet = "Error-data")
-  return(df)
+  return(fetch_sheet("Error-data"))
 }
 
 fetch_german_data <- function(){
-  df_german <- read_sheet(GS_SHEET, "Original-german")
-  return(df_german)
+  return(fetch_sheet("Original-german"))
+}
+
+fetch_sheet <- function(sheet_name,...){
+  sh <- read_sheet(ss=GS_SHEET, sheet=sheet_name,...)
+  return(sh)
 }
 
 #' Updates the list 
@@ -38,4 +46,8 @@ update_question_categories <- function(df_all){
   colnames(df_new_questions) <- colnames(df_categories)
   df_new_questions[,1] <- new_questions
   sheet_append(GS_SHEET, df_new_questions, sheet = "Question-categories")
+}
+
+update_work_categories <- function(categories){
+  sheet_
 }
