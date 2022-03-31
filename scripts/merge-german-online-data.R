274abb9e0907cfbde01c6f3a166d4e29b29717e4
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-03-31 13:44:19 +0200

    fixes german online merger

--- scripts/merge-german-online-data.R
+++ scripts/merge-german-online-data.R
@@ -1,7 +1,8 @@
 # This expects the xlsx file to be present
 
 df_german <- df_german %>%
-  select(-c(giveaway, interview_consent, longterm_consent, charity))
+  select(-c(giveaway, interview_consent, longterm_consent, 
+            charity, partner_widow_age))
 
 # Adding lcis columns
 df_german <- df_german %>%
@@ -11,7 +12,13 @@ df_german <- df_german %>%
   add_column(lcis7_freq = NA, .after = "lcis7") %>% 
   add_column(lcis12_freq = NA, .after = "lcis12")
 
-df_german <- select(df_german, -partner_widow_age)
 colnames(df_german) <- colnames(select(df_online, -ident))
+df_german <- df_german %>%
+  mutate(vek=as.character(vek),
+         wid_len=as.character(wid_len)) %>%
+  mutate_at(vars(starts_with("lcis")), ~as.character(.))
 
-bind_rows(df_german, df_online)
+df_online <- df_online %>%
+  mutate_at(vars(starts_with("lcis")), ~as.character(.))
+
+df_online <- bind_rows(df_german, df_online)
