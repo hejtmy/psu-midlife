85d5710cab5517f03cb83571502222aebb7fe225
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-03-31 13:44:30 +0200

    Updates buffering

--- scripts/buffer-data.R
+++ scripts/buffer-data.R
@@ -1,18 +1,20 @@
 #' This loads, processes and saves data so that they are prepared for the shiny app and 
 #' reports
 library(googlesheets4)
+library(tidyverse)
 source("functions/fetch-online-data.R")
 
 df_online <- fetch_data_online()
 df_paper <- fetch_data_paper()
-
-source("scripts/merge-german-online-data.R")
 df_german <- fetch_german_data()
 
+source("scripts/merge-german-online-data.R")
 source("scripts/merge-paper-online-data.R")
 
 dir.create("processed", showWarnings = FALSE)
 write.table(df_all, "processed/all-data-raw.csv", sep=";", row.names = FALSE)
+
+source("scripts/process-data.R")
 
 df_question_categories <- fetch_question_categories() 
 write.table(df_question_categories, "processed/question-categories.csv", sep=";")
--- scripts/process-data.R
+++ scripts/process-data.R
@@ -3,4 +3,5 @@ source("functions/fetch-online-data.R")
 df_error <- fetch_error_data()
 df_processed <- process_data(df_all, df_error)
 dir.create("processed", showWarnings = FALSE)
-write.table(df_processed, "processed/all-data-processed.csv", sep=";", row.names = FALSE)
+write.table(df_processed, "processed/all-data-processed.csv", sep=";",
+            row.names = FALSE)
