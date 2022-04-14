97c6943345f85500dee27879c946019104f8cbaf
Lukáš Hejtmánek <hejtmy@gmail.com>
2022-04-14 11:58:48 +0200

    Fixes issues with buffering and loading

--- functions/fetch-online-data.R
+++ functions/fetch-online-data.R
@@ -2,9 +2,8 @@ library(googlesheets4)
 
 GS_SHEET <- "1pPdD8Q3r9DLZuKAfFrm95W14GHbUw6NOIipgxYJRUeI"
 
-fetch_data_paper <- function(){
-  df_paper <- read_sheet(ss = GS_SHEET,sheet = "Original-paper", na = c("na", "nr"))
-  return(df_paper)
+fetch_data_paper <- function(use_buffered=TRUE){
+  return(fetch_sheet("Original-paper", use_buffered, na = c("", "na", "nr")))
 }
 
 fetch_data_paper_edited <- function(use_buffered=TRUE){
@@ -31,14 +30,16 @@ fetch_german_data <- function(use_buffered=TRUE){
 }
 
 fetch_sheet <- function(sheet_name, use_buffered, ...){
-  filepath <- paste0(file.path("data", "buffered", sheet_name), ".csv",
+  filepath <- paste0(file.path("data", "buffered", sheet_name), ".RData",
                      collapse = "")
   if(use_buffered & file.exists(filepath)){
-    sh <- read.table(filepath, sep=";", header=TRUE)
+    load(filepath)
   } else {
     dir.create(dirname(filepath), showWarnings = FALSE)
-    sh <- read_sheet(ss=GS_SHEET, sheet=sheet_name, ...)
-    write.table(sh, filepath, sep=";")
+    sh <- read_sheet(ss=GS_SHEET, sheet=sheet_name, guess_max = 25, ...)
+    sh <- mutate(sh, across(where(is.list), as.character))
+    save(sh, file = filepath)
+    #write.table(sh, file = filepath, sep=";", row.names = FALSE, fileEncoding = "UTF-8")
   }
   return(sh)
 }
@@ -50,7 +51,8 @@ update_question_categories <- function(df_all){
   df_categories <- fetch_question_categories()
   existing_questions <- df_categories[[1]]
   new_questions <- setdiff(colnames(df_all), existing_questions)
-  df_new_questions <- data.frame(matrix(ncol = ncol(df_categories), nrow = length(new_questions)))
+  df_new_questions <- data.frame(matrix(ncol = ncol(df_categories), 
+                                        nrow = length(new_questions)))
   colnames(df_new_questions) <- colnames(df_categories)
   df_new_questions[,1] <- new_questions
   sheet_append(GS_SHEET, df_new_questions, sheet = "Question-categories")
